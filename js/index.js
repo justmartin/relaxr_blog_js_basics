62d0fb41af8daffd694bdfd3d806f1ad79594175
justmartin <justinmartin@Justins-MacBook-Pro-2.local>
2015-10-27 17:19:32 -0700

    added prevent default to functions

--- js/index.js
+++ js/index.js
@@ -3,25 +3,30 @@ $(document).ready(function() {
 	$('.readmore').click(readMoreText);
 	$('.readless').click(readLessText);
 	$('.learnmore').click(learnMoreText);
-	$('a').click(preventLinkJump);
+	//$('a').click(preventLinkJump);
 
+	/*
 	function preventLinkJump(event) {
 	  event.preventDefault();
 	}
+	*/
 
-	function readMoreText(){
+	function readMoreText(event){
+		event.preventDefault();
 		$('.readmore').hide();
 		$('#show-this-on-click').slideDown();
 		$('.readless').show();
 	}
 
-	function readLessText(){
+	function readLessText(event){
+		event.preventDefault();
 		$('.readless').hide();
 		$('#show-this-on-click').slideUp();
 		$('.readmore').show();
 	}
 
-	function learnMoreText(){
+	function learnMoreText(event){
+		event.preventDefault();
 		$('.learnmore').hide();
 		$('#learnmoretext').slideDown();
 	}
